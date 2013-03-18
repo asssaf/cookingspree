7c2947364aa95bf82e0fe099a0bdb1b8b44f9c8b
assaf <assaf>
2013-03-18 18:27:01 +0200

    add ingredient selection and search url

--- ingredient.js
+++ ingredient.js
@@ -1,17 +1,27 @@
 function IngredientCtrl($scope) {
   // default content
   $scope.ingredients = [
-    {name:'Avocado'}
+    {name:'Avocado', selected:true}
   ];
   
   // add a new ingredient
   $scope.addIngredient = function() {
-    $scope.ingredients.push({name:$scope.ingredientName});
+    $scope.ingredients.push({name:$scope.ingredientName, selected:true});
     $scope.ingredientName = '';
   };
   
   // clear ingredient list
   $scope.clear = function() {
     $scope.ingredients = [];
+  };
+  
+  $scope.searchUrl = function() {
+    var query = ""
+    angular.forEach($scope.ingredients, function(ingredient) {
+      if (ingredient.selected) {
+	query += ingredient.name + "+";
+      }
+    });
+    return "http://www.google.com/search?q=" + query;
   };
 }
